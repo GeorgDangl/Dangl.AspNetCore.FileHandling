23346a1d2e710749d19dbfc9d0e87b20ea0faba3
Georg Dangl <georg@dangl.me>
2018-06-01 22:35:08 +0200

    Update defaults handling

--- src/Dangl.AspNetCore.FileHandling/FileHandlerDefaults.cs
+++ src/Dangl.AspNetCore.FileHandling/FileHandlerDefaults.cs
@@ -3,7 +3,7 @@
     public static class FileHandlerDefaults
     {
         public const int FILE_CONTAINER_NAME_MAX_LENGTH = 63;
-        public const int FILE_CONTAINER_NAME_MIN_LENGTH = 2;
+        public const int FILE_CONTAINER_NAME_MIN_LENGTH = 3;
         public const int FILE_PATH_MAX_LENGTH = 1024;
     }
 }
--- src/Dangl.AspNetCore.FileHandling/RelativeFilePathBuilder.cs
+++ src/Dangl.AspNetCore.FileHandling/RelativeFilePathBuilder.cs
@@ -34,7 +34,7 @@ namespace Dangl.AspNetCore.FileHandling
             var isValid = Regex.IsMatch(container, containerNameRegex, RegexOptions.Compiled);
             return isValid
                    && container.Length <= FileHandlerDefaults.FILE_CONTAINER_NAME_MAX_LENGTH
-                   && container.Length > FileHandlerDefaults.FILE_CONTAINER_NAME_MIN_LENGTH;
+                   && container.Length >= FileHandlerDefaults.FILE_CONTAINER_NAME_MIN_LENGTH;
         }
     }
 }
