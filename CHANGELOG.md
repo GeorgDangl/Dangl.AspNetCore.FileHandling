c11ac7b40cffae5c988cec5b1858c90d33485cd5
Georg Dangl <georg@dangl.me>
2018-06-07 22:14:41 +0200

    Update InMemoryFileManager

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 All notable changes to **Dangl.AspNetCore.FileHandling** are documented here.
 
+## v0.1.3:
+- Update `InMemoryFileManager` to create copies of the saved streams so they are accessible after the original was disposed. The `Clear()` method on the `InMemoryFileManager` now disposes all held streams before releasing them
+
 ## v0.1.2:
 - Internal update of build system
 
---
+++ test/Dangl.AspNetCore.FileHandling.Tests/InMemoryFileManagerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dangl.AspNetCore.FileHandling.Tests
+{
+    public class InMemoryFileManagerTests
+    {
+        [Fact]
+        public async Task CanAccessSavedStreamEvenWhenOriginalIsDisposed()
+        {
+            var inMemoryFileManager = new InMemoryFileManager();
+            using (var originalMemStream = new MemoryStream(new byte[] { 1, 2, 3, 4, 5 }))
+            {
+                await inMemoryFileManager.SaveFileAsync(Guid.NewGuid(), "my-container", "file.bin", originalMemStream);
+            }
+            var savedFile = InMemoryFileManager.SavedFiles.Single();
+            Assert.Equal(5, savedFile.FileStream.Length); // If it can be accessed, everything's fine
+        }
+    }
+}
